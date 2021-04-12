34de7a2aa6c7b58c402fda2c3eb67db76af934a7
Guilherme Maciel <guilherme.petena@gmail.com>
2021-04-12 17:44:01 -0300

    Update TaskService.java

--- task-planning/src/main/java/com/planning/taskplanning/service/TaskService.java
+++ task-planning/src/main/java/com/planning/taskplanning/service/TaskService.java
@@ -1,18 +1,36 @@
-package com.planning.taskplanning.service;
+package com.planning.projectissue.service;
 
-import com.planning.taskplanning.model.Task;
-import org.springframework.stereotype.Service;
+import com.planning.projectissue.model.Task;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
+@Component
 public class TaskService {
 
     public static List<Task> taskList = new ArrayList<>();
 
-    public List<Task> adicionarTaskNaLista(Task task){
+    public List<Task> addList(Task task){
         taskList.add(task);
         return taskList;
+    }
+
+    public List<Task> addListWithPosition(Task task, int index){
+        taskList.add(index, task);
+        return taskList;
+    }
+    public List<Task> getAll(){
+        return taskList;
+    }
+    public Task getTaskByIndex(int index){
+        return taskList.get(index);
+    }
+    public Task getTaskByObject(Task task){
+        return taskList.get(taskList.indexOf(task));
+    }
+
+    public Task remove(int index){
+        return taskList.remove(index);
     }
 }
