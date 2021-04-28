99587dd00d7f1ff4f0ffb9f93ce074bc78fde456
Guilherme Maciel <guilherme.petena@gmail.com>
2021-04-28 16:21:51 -0300

    Update Excel.java

--- task-planning/src/main/java/com/planning/taskplanning/model/Excel.java
+++ task-planning/src/main/java/com/planning/taskplanning/model/Excel.java
@@ -1,22 +1,52 @@
 package com.planning.taskplanning.model;
 
-public class Excel {
-    private String caminho;
+@Entity
+public class Story {
 
-    public Excel() {
+    @Id
+    private UUID id = UUID.randomUUID();
+    private String title;
+    private String storyNumber;
+
+    public Story() {
     }
 
-    public Excel(String caminho) {
-        this.caminho = caminho;
+    public Story(UUID id, String title, String storyNumber) {
+        this.id = id;
+        this.title = title;
+        this.storyNumber = storyNumber;
     }
 
-    public String getCaminho() {
-        return caminho;
+    public UUID getId() {
+        return id;
     }
 
-    public void setCaminho(String caminho) {
-        this.caminho = caminho;
+    public void setId(UUID id) {
+        this.id = id;
     }
-}
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStoryNumber() {
+        return storyNumber;
+    }
+
+    public void setStoryNumber(String storyNumber) {
+        this.storyNumber = storyNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Story{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", storyNumber='" + storyNumber + '\'' +
+                '}';
+    }
 
