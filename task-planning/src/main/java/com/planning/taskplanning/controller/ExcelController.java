fe5c5cdb830d5dd3f5b36168096faa4277f989f2
Guilherme Maciel <guilherme.petena@gmail.com>
2021-04-28 16:18:36 -0300

    Update ExcelController.java

--- task-planning/src/main/java/com/planning/taskplanning/controller/ExcelController.java
+++ task-planning/src/main/java/com/planning/taskplanning/controller/ExcelController.java
@@ -10,16 +10,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
-@RequestMapping("/excel")
-public class ExcelController {
+@RequestMapping("/story")
+public class StoryController {
 
     @Autowired
-    private ExcelService excelService;
+    private StoryService storyService;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void create(Excel excel) {
-         excelService.processar(excel, TaskService.taskList);
+    @GetMapping()
+    public ResponseEntity<List<Story>> getAll() {
+        return ResponseEntity.ok(storyService.listarStories());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Story> findOne(@PathVariable("id") UUID id) {
+        if(storyService.storyExiste(id)){
+            return ResponseEntity.ok(storyService.obterStory(id));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping()
+    public ResponseEntity<Story> create(@RequestBody Story story) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(storyService.criarStory(story));
     }
 }
