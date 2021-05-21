9c7e3bb37b21ad77caee56fe7d26d2463c72727d
Guilherme Maciel <guilherme.petena@gmail.com>
2021-05-21 18:02:46 -0300

    Update StoryRepository.java

--- task-planning/src/main/java/com/planning/taskplanning/repository/StoryRepository.java
+++ task-planning/src/main/java/com/planning/taskplanning/repository/StoryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface StoryRepository extends JpaRepository<Story, UUID> {
+public interface StoryRepository extends JpaRepository<Story, String> {
 }
